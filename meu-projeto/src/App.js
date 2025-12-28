8deea687f2191fc4210787685a6cdf78be22f1c3
Leonardo-de-Moura <franscisco.tico.fssm@gmail.com>
2025-12-28 10:46:21 -0300

    up

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,14 +1,18 @@
 
-import './App.css';
-
-import HelloWorld from './components/HelloWorld';               
-
+import './App.css';         
+import SayMyName from './components/SayMyName';  
+import Pessoa from './components/Pessoa';
 
 function App() {
+  const nome = "maria"
  
   return (
     <div className="App">
-    <HelloWorld/>
+    <SayMyName nome="matheus"/>
+    <SayMyName nome={nome}/>
+<Pessoa
+nome ="rodrigo" foto ="https://img" idade="28" profissao="pedreiro"
+/>
     </div>
   );
 }
